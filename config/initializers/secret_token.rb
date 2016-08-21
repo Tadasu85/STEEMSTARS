13895ee7f616bdc3d2c6633919a57bca967277f6
Tadasu85 <johnstor@gmail.com>
2016-08-21 16:59:25 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+STEEMSTARS::Application.config.secret_token = '48df54371f9662e3ac2b7d9b4873d22e67a9f6f37fe4df01b0c389ce9337a6d5f4d0f4005ca5a6b9e425a319f9007fc4408dcf37bf1b5f86f6af9642fdd529ed'
