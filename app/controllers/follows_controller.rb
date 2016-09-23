225c5b1bf5b88657b43c7e67a568ca4dedceb18c
Raymon Johnstone <johnstor@gmail.com>
2016-09-23 21:59:26 +0000

    added follow controller index

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -8,7 +8,7 @@ class FollowsController < ApplicationController
     until count == @follows.size
       count = @follows.size
       response = api.get_follows(@account_id, @follows.last, 'blog', 100)
-      @follows += response.result.map(&:follower)
+      @follows += response.result.map(&:follow)
       @follows = @follows.uniq
     end
   end
