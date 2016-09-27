4f40e961ada112e8e823c0e697e0216fccf4d2c1
Raymon Johnstone <johnstor@gmail.com>
2016-09-27 18:22:32 +0000

    PG creation and Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160823235119) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "user"
     t.datetime "created_at"
