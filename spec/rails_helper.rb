3783702d650fbcdfd7d68691cab0cabe6d40fff3
Raymon Johnstone <johnstor@gmail.com>
2016-12-03 03:37:07 +0000

    Rspec was not configing devise tester properly.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,3 +55,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+end
