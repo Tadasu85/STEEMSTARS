33b3848dbd4532b7103c15d91f556a3c0b1bf0da
Raymon Johnstone <johnstor@gmail.com>
2016-12-14 21:47:45 +0000

    Thanks to connor for finding the redis var and redis global vars.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 require 'redis'
 # config/initializers/redis.rb
-location = ENV["REDISCLOUD_URL"] || 'redis://127.0.0.1:6379/0'
+location = ENV["REDISTOGO_URL"] || 'redis://127.0.0.1:6379/0'
 uri = URI.parse(location)
 $redis = Redis.new(:url => uri)
 
