defc1cb5f7736154ebec0d1c0898affbcf963fe9
Raymon Johnstone <johnstor@gmail.com>
2017-05-11 05:26:44 +0000

    Cleanup and check for publish.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-  #devise_parameter_sanitizer.permit(:sign_up, keys: [:user])
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:user])
   devise_parameter_sanitizer.permit(:sign_up) do |user_params|
     user_params.permit(:steemaccount, :email, :password, :current_password, :password_confirmation)
   end
